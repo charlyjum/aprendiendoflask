f5e537d3a80719e04e9a8c1573383098249168b1
Carlos Perez <charlyjun@gmail.com>
2020-04-23 20:41:50 -0500

    decorador y funcion, ok

--- ServidorFlask.py
+++ ServidorFlask.py
@@ -1,4 +1,8 @@
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__) #creamos la instancia de la clase
 
+#usamos el decorador router para decirle que URL debe activar nuestra funcion
+@app.route('/')
+def hello_world():
+    return 'Hello,World!'
