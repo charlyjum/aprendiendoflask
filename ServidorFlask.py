ccbc0cad11dacaad8c98d82ba2b2e1f0c6f98529
Carlos Perez <charlyjun@gmail.com>
2020-04-24 12:51:09 -0500

    app run con debug

--- ServidorFlask.py
+++ ServidorFlask.py
@@ -6,3 +6,10 @@ app = Flask(__name__) #creamos la instancia de la clase
 @app.route('/')
 def hello_world():
     return 'Hello,World!'
+
+#Una aplicacion web siempre debe de estar escuchando peticiones, se hace asi
+if __name__ == '__main__': #se hace una validacion para comprobar si
+                           #se esta en el archivo principal para ver si es el
+                           #archivo de ejecucion y no un modulo
+    app.run(debug=True, host="10.100.100.10", port=9566) #indica que la aplicacion estara en prueba y requiero que se
+                        #reinicie cada vez que modifique algo.
