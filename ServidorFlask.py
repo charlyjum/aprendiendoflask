adc3415b6bae6670aebc985b6fc1ec9c52cc1376
Carlos Perez <charlyjun@gmail.com>
2020-04-24 19:40:48 -0500

    routing

--- ServidorFlask.py
+++ ServidorFlask.py
@@ -4,8 +4,12 @@ app = Flask(__name__) #creamos la instancia de la clase
 
 #usamos el decorador router para decirle que URL debe activar nuestra funcion
 @app.route('/')
-def hello_world():
-    return 'Hello,World!'
+def index():
+    return 'Pagina Principal'
+
+@app.route('/hello') #Use el decorador route para vincular una función a una URL
+def hello():
+    return 'Hola Mundo'
 
 #Una aplicacion web siempre debe de estar escuchando peticiones, se hace asi
 if __name__ == '__main__': #se hace una validacion para comprobar si
