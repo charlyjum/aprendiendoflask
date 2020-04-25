be93b1bc4243e780324c582c5f24cfc33f22c17d
Carlos Perez <charlyjun@gmail.com>
2020-04-24 19:57:49 -0500

    render_plate

--- ServidorFlask.py
+++ ServidorFlask.py
@@ -1,5 +1,6 @@
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__) #creamos la instancia de la clase
 
 #usamos el decorador router para decirle que URL debe activar nuestra funcion
@@ -9,7 +10,7 @@ def index():
 
 @app.route('/hello') #Use el decorador route para vincular una función a una URL
 def hello():
-    return 'Hola Mundo'
+    return render_template('hello.html')
 
 #Una aplicacion web siempre debe de estar escuchando peticiones, se hace asi
 if __name__ == '__main__': #se hace una validacion para comprobar si
